139f8cc4f8006d070eaa9c6e78218c5f4c5c3697
Danpl <styoda6@gmail.com>
2026-09-18 15:25:24 +0300

    Structure files Added: matplot graphic about how many chars in text

--- explore.py
+++ explore.py
@@ -41,6 +41,7 @@ def plot_letter_frequency(top_letters):
 
 def main():
     text = load_and_clean(DATASET_PATH)
+
     vocab_size, chars, ch2i, i2ch = build_vocab(text)
 
     print_basic_stats(text, vocab_size, chars)
